40079862acbbcd8f159f260ffedf5ac90b945694
illiaBabak <illiababak776@gmail.com>
2024-03-09 00:05:01 +0100

    Added README.md

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,9 @@
 export const Loader = (): JSX.Element => (
   <div className='loading-content'>
-    <img src='https://media.tenor.com/S5dmpIeLM-oAAAAM/punjabi-airpod-stare.gif' className='loading-img' />
+    <img
+      src='https://static.vecteezy.com/system/resources/previews/011/571/519/non_2x/circle-flag-of-india-free-png.png'
+      className='loading-img'
+    />
     <p className='loading-text'>Loading</p>
   </div>
 );
