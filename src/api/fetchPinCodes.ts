e01e1641d424b63fb9bf1d835cb0cbaf08d46130
illiaBabak <illiababak776@gmail.com>
2024-09-17 23:46:56 +0200

    Update fetch options

--- src/api/fetchPinCodes.ts
+++ src/api/fetchPinCodes.ts
@@ -1,24 +1,21 @@
 import { PinCode } from 'src/types/pinCodesData';
 import { isPinCodeArr } from 'src/utils/guards';
 
-const url = 'https://pincode.p.rapidapi.com/';
+const url = 'https://indian-pincode-2024.p.rapidapi.com/';
 const apiKey = 'a7c94cd0d5msh6c4aec45fd9f63fp132de7jsndf7645f754b9';
-const apiHost = 'pincode.p.rapidapi.com';
+const apiHost = 'indian-pincode-2024.p.rapidapi.com';
 
 export const fetchPinCodes = async (pinCodeNumber: number): Promise<PinCode[]> => {
   const options = {
-    method: 'POST',
+    method: 'GET',
     headers: {
-      'content-type': 'application/json',
-      'Content-Type': 'application/json',
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': apiHost,
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': apiHost,
     },
-    body: JSON.stringify({ searchBy: 'pincode', value: pinCodeNumber }),
   };
 
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(`${url}?pincode=${pinCodeNumber}`, options);
     const result: unknown = await response.json();
 
     if (isPinCodeArr(result)) return result;
--- src/api/fetchTrains.ts
+++ src/api/fetchTrains.ts
@@ -1,7 +1,7 @@
 import { TrainData } from 'src/types/trainData';
 import { isTrainArr } from 'src/utils/guards';
 
-const url = 'https://trains.p.rapidapi.com/';
+const url = 'https://trains.p.rapidapi.com/v1/railways/trains/india/';
 const apiKey = 'a7c94cd0d5msh6c4aec45fd9f63fp132de7jsndf7645f754b9';
 const apiHost = 'trains.p.rapidapi.com';
 
@@ -9,9 +9,9 @@ export const fetchTrains = async (text: string): Promise<TrainData[]> => {
   const options = {
     method: 'POST',
     headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': apiHost,
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': apiHost,
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ search: text }),
   };
